9638e00ea452343dac5715433eb15740fd27ce41
yumamur <pccartman@gmail.com>
2023-10-06 12:39:43 +0300

    typeft

--- deneme/main.c
+++ deneme/main.c
@@ -1,10 +1,33 @@
 #include <stdio.h>
 #include <unistd.h>
 
-int main()
-{
-	int		i;
-	char	str[] = "qqqaaa";
+typedef char	t_byte7[7];
 
-	i = -1;
+int	func(t_byte7 **a, int i)
+{
+	// static int	ctl;
+
+	if (!i)
+	// {
+	// 	++ctl;
+		*a = &(t_byte7){'a', 'a', 'a', 'a', 'a', 'a', 0};
+	// }
+	else
+		*a = &(t_byte7){'b', 'b', 'b', 'b', 'b', 'b', 0};
+	return (0);
+}
+
+int	main(void)
+{
+	t_byte7	*a;
+	t_byte7	*b;
+
+	// for (int i = 0; i < 100; ++i)
+	// 	printf("%d\n", i * i + i);
+	func(&a, 0);
+	// for (int i = 0; i < 100; ++i)
+	// 	printf("%d\n", i * i + i);
+	func(&b, 1);
+	printf("%s\n", *a);
+	printf("%s\n", *b);
 }
